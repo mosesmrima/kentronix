57c24739b09bdad90900352127918fa3e3f65406
mrima <mrimamss@gmail.com>
2023-01-30 10:14:22 +0300

    index

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import Head from "next/head"
 export default function Home() {
   return (
     <>
-     <h1>hello</h1>
+        <Head>
+            <title>Kentronix</title>
+            <link rel={"icon"} href={"/favicon.png"}/>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        </Head>
     </>
   )
 }
