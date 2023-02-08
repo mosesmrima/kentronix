fc5ccf9df22f0e229ae65c4eef336f52ca5c4b32
mrima <mrimamss@gmail.com>
2023-02-08 10:06:39 +0300

    remove navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,11 @@
 import '../styles/globals.css'
 import '../components/NavBar'
-import {NavBar} from "../components/NavBar";
+
 
 export default function App({ Component, pageProps }) {
   return (
       <>
         <div  className={"w-screen bg-gradient-to-r from-slate-50 to-blue-500"}>
-            <NavBar/>
             <Component {...pageProps} />
         </div>
       </>
