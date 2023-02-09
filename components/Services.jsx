944c28012fddfc9d2964292d2ef96f6809559827
mrima <mrimamss@gmail.com>
2023-02-09 09:49:14 +0300

    add padding to lg carousel

--- components/Services.jsx
+++ components/Services.jsx
@@ -11,7 +11,7 @@ export default function Services() {
         <>
             <div className={"p-0 m-0 w-[100vw] h-[70vh] overflow-hidden flex flex-col justify-center items-center"}>
                 <h1>Services</h1>
-                <motion.div ref={carousel} className={"carousel cursor-grab w-10/12 overflow-hidden flex rounded-full"}>
+                <motion.div ref={carousel} className={"carousel cursor-grab w-10/12 overflow-hidden flex rounded-full sm:px-12"}>
                     <motion.div drag={"x"} dragConstraints={{right: 0, left: -width}} className={"inner-carousel flex flex-shrink-0  w-12/12 h-[400px] justify-center items-center "}>
                         {
                             [1,2,3,4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map((el) => (
