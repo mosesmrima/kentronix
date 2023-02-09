511f7ab22ced9b776c0e23daf966009ba3e5911d
mrima <mrimamss@gmail.com>
2023-02-09 09:40:52 +0300

    rounded carousel

--- components/Services.jsx
+++ components/Services.jsx
@@ -11,11 +11,11 @@ export default function Services() {
         <>
             <div className={"p-0 m-0 w-[100vw] h-[70vh] overflow-hidden flex flex-col justify-center items-center"}>
                 <h1>Services</h1>
-                <motion.div ref={carousel} className={"carousel cursor-grab w-10/12 overflow-hidden flex"}>
+                <motion.div ref={carousel} className={"carousel cursor-grab w-10/12 overflow-hidden flex rounded-full"}>
                     <motion.div drag={"x"} dragConstraints={{right: 0, left: -width}} className={"inner-carousel flex flex-shrink-0  w-12/12 h-[400px] justify-center items-center "}>
                         {
                             [1,2,3,4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map((el) => (
-                                <motion.div key={el} className={"p-8 rounded-3xl carousel-item w-52 h-60 min-w-28 bg-green-300 m-4 flex-shrink-0 pointer-events-none"}>
+                                <motion.div key={el} className={"p-8 rounded-3xl carousel-item w-[30vw] h-[50vh] min-w-28 bg-green-300 m-4 flex-shrink-0 pointer-events-none"}>
                                     {el}
                                 </motion.div>
                             ))
