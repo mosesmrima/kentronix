2269c1bd6bbb96bff3818a23f5780dedd090c219
mrima <mrimamss@gmail.com>
2023-02-13 12:28:07 +0300

    copy

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -2,8 +2,8 @@ export default function Contact() {
     return (
         <div className={"m-16 text-center"}>
             <h1 className={"m-8"}>Contacts:</h1>
-            <h3>Email: kentronix@sales.com</h3>
-            <h3>Copyright &copy; Kentronix.tech 2023</h3>
+            <h3>Email: sales@kentronix.com</h3>
+            <h3>Copyright &copy; Kentronix Tech International Ltd. 2023</h3>
         </div>
     );
 }
