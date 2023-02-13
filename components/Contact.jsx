dac2c5550806170dfe5d0d53fb0ba57551eca35c
mrima <mrimamss@gmail.com>
2023-02-13 12:22:22 +0300

    contacts

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,7 +1,7 @@
 export default function Contact() {
     return (
         <div className={"m-16 text-center"}>
-            <h1 className={"m-8"}>Contact Us on:</h1>
+            <h1 className={"m-8"}>Contacts:</h1>
             <h3>Email: kentronix@sales.com</h3>
             <h3>Copyright &copy; Kentronix.tech 2023</h3>
         </div>
