d915e1589ab9f334b3ea64ee9666d0ae62e79c06
mrima <mrimamss@gmail.com>
2023-03-20 16:40:04 +0300

    fix

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,6 +1,6 @@
 export default function Contact() {
     return (
-        <div className={"m-16 text-center bg-pink-400 p-8 2 rounded-2xl  appearance-non rounded-3xl flex flex-col justify-center items-center min-h-[250px] "}>
+        <div className={ "min-w-[300px] m-16 text-center bg-pink-400 p-8 2 rounded-2xl  appearance-non rounded-3xl flex flex-col justify-center items-center min-h-[250px] "}>
             <h3 className={"mb-16"}>Contact Us</h3>
             <p>Email: sales@kentronix.com</p>
             <p>Copyright &copy; Kentronix Tech International Ltd. 2023</p>
